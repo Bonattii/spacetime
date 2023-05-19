c1a47959288f85c9a314bb5820108208b363a8d9
Rodrigo Bonatti <bonattirodrigo123@hotmail.com>
2023-05-18 17:06:14 -0700

    Add a middleware to protect the memories routes

---
+++ web/src/middleware.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const signInURL = `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`;
+
+export const middleware = (request: NextRequest) => {
+  // Get the value of the token cookie
+  const token = request.cookies.get("token")?.value;
+
+  // If the user is not logged in, will redirect to the login page
+  if (!token) {
+    return NextResponse.redirect(signInURL);
+  }
+
+  // Just continue
+  return NextResponse.next();
+};
+
+// Middleware will be called when any memories route try to be accessed
+export const config = {
+  matcher: "/memories/:path*",
+};
